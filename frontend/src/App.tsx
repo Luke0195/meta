3222c53e4371e0ac1f0d4dece548da64ee0dee0a
Lucas <lucas201273@gmail.com>
2022-07-11 21:44:32 -0300

    feat: first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import NotificationButton from './components/NotificationButton'
 import './App.css'
 
 const App = () =>{
   return(
-    <>
+    <React.Fragment>
       <h1> Ds Meta</h1>
-    </>
+      <NotificationButton/>
+    </React.Fragment>
   )
 }
 
